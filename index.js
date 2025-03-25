bb61afed8bcbfa60eb46adf98b6c77aa7f01ece3
Karan-Bharti1 <karan.codes1@gmail.com>
2025-03-25 14:14:58 +0530

    Creating a jwt token

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express=require("express")
+const jwt=require("jsonwebtoken")
 const app=express()
 app.use(express.json())
 const SECRET_KEY="supersecretadmin"
+const JWT_SECRET='your_jwt_secret'
 app.post("/admin/login",(req,res)=>{
     const {secret}=req.body
     if(secret===SECRET_KEY){
-        res.json({message:"Access Granted"})
+        const token=jwt.sign({role:"admin"},JWT_SECRET,{expiresIn:"24h"})
+        res.json({token})
     }else{
         res.json({message:"Invalid Secret"})
     }
 })
 app.listen(5000,()=>{
-    console.log("App Running at portal 5000")
+    console.log("App Running at port 5000")
 })
