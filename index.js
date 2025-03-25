fc37c3563388554dc1761f5135a8ffc665e633a2
Karan-Bharti1 <karan.codes1@gmail.com>
2025-03-25 14:28:20 +0530

    creating middleware to verify jwt token before allowing access to protected route

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const app=express()
 app.use(express.json())
 const SECRET_KEY="supersecretadmin"
 const JWT_SECRET='your_jwt_secret'
+const verifyJWT=(req,res,next)=>{
+    const token=req.headers['authorization']//we will pass headers in the fronend that will contain key as 'authorization
+    if(!token){
+        return res.status(401).json({message:"No token provided"})
+    }
+    try {
+    //  const decodedtOKEN=jwt.verify(token)
+     console.log(token)   
+    }catch(error){
+        return res.status(402).json({message:"Invalid Token"})
+    }
+}
 app.post("/admin/login",(req,res)=>{
     const {secret}=req.body
     if(secret===SECRET_KEY){
@@ -13,6 +25,7 @@ app.post("/admin/login",(req,res)=>{
         res.json({message:"Invalid Secret"})
     }
 })
+
 app.listen(5000,()=>{
     console.log("App Running at port 5000")
 })
